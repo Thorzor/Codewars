03dce89386475b62b51fc3debf2520ac2259bbe0
unknown <dima.sufranovich@gmail.com>
2023-02-18 22:20:40 +0300

    add select arrat reversing

--- select_array_reversing.py
+++ select_array_reversing.py
@@ -1,3 +1,36 @@
 def sel_reverse(arr, l):
-    # your code here
-    pass
+    new_arr = []
+
+    if l > len(arr):
+        arr.reverse()
+        return arr
+
+    if l == 0:
+        return arr
+
+    while True:
+        if len(arr) != 0:
+
+            if l >= len(arr):
+                counter = len(arr)
+            else:
+                counter = l
+
+            values = []
+            count = counter
+
+            for i in range(counter):
+                values.append(arr[i])
+                counter -= 1
+
+            values.reverse()
+
+            for y in values:
+                new_arr.append(y)
+
+            for x in range(count):
+                arr.pop(0)
+
+        else:
+            break
+    return new_arr
