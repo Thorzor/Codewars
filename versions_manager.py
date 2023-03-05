061fe98fb8ae7c3103c494a0a03a1652795f50ef
unknown <dima.sufranovich@gmail.com>
2023-03-05 20:07:44 +0300

    added part of solution for version manager

--- versions_manager.py
+++ versions_manager.py
@@ -1,5 +1,29 @@
-a = "1.2.3.4".split(".")
-print(a)
+class VersionManager:
+    def __init__(self, version=None):
+        if version == "" or version is None:
+            version = "0.0.1"
+            self.version = version
+        else:
+            self.version = version
 
-for i in a:
-    print(i)
+    def release(self):
+        return self.version
+
+    def major(self):
+        values = [x for x in self.version]
+        values[0] = str(int(values[0]) + 1)
+        major_version = ''.join([str(elem) for elem in values])
+        return major_version
+
+    def minor(self):
+        pass
+
+    def patch(self):
+        pass
+
+    def rollback(self):
+        pass
+
+
+a = VersionManager("0.0.1")
+print(a.major())
